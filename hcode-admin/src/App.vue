ab8f71767da7ecb06360110f4f528c71c5bff3b2
Anthony Rafael <anthonyribeiro1910@gmail.com>
2020-09-16 02:17:30 -0300

    Primeiro Projeto Vuetify

---
+++ hcode-admin/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <AdminForm />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AdminForm from './components/AdminForm';
+
+export default {
+  name: 'App',
+  components: {
+    AdminForm,
+  },
+};
+</script>
